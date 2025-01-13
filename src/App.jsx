605d6048498ae20b18879765d86d9b999960fd4a
sahil16-12 <sahil16december@gmail.com>
2025-01-13 19:54:13 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <h1>A blog wirh me {import.meta.env.VITE_APPWRITE_DATABASE_ID}</h1>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
